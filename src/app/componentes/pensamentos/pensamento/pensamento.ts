314bdff21664007917ce52b2eeae0b3832fe3d1e
DennerOl <dener155@hotmail.com>
2024-06-05 21:57:26 -0300

    Interface para trafegar dados p/ back-end

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.ts
@@ -0,0 +1,9 @@
+/*interface responsavel pelo formato do obj
+que vai ser passado para o back-end */
+
+export interface Pensamento {
+  id: number;
+  conteudo: string;
+  autoria: string;
+  modelo: string;
+}
