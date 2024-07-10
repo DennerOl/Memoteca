6ff3105c7da4605629dd6b5c016d385ebadc5356
DennerOl <dener155@hotmail.com>
2024-07-09 21:55:20 -0300

    Consumindo APIViaCep no Form

--- forms-angular-main/src/app/app.module.ts
+++ forms-angular-main/src/app/app.module.ts
@@ -8,8 +8,10 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { CadastroComponent } from "./cadastro/cadastro.component";
 import { SucessoCadastroComponent } from "./sucesso-cadastro/sucesso-cadastro.component";
 import { FormsModule } from "@angular/forms";
-import { MensagemComponent } from './components/mensagem/mensagem.component';
-import { MaiorIdadeDirective } from './directives/maior-idade.directive';
+import { MensagemComponent } from "./components/mensagem/mensagem.component";
+import { MaiorIdadeDirective } from "./directives/maior-idade.directive";
+import { HttpClientModule } from "@angular/common/http";
+import { ValidandoCepDirective } from './directives/validando-cep.directive';
 
 @NgModule({
   declarations: [
@@ -20,8 +22,9 @@ import { MaiorIdadeDirective } from './directives/maior-idade.directive';
     SucessoCadastroComponent,
     MensagemComponent,
     MaiorIdadeDirective,
+    ValidandoCepDirective,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
